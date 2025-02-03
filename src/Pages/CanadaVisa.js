f70d22d8ba0044c1fd3812ebc97095f7339f5453
Muhammad Haroon <haroonmajid0123@gmail.com>
2025-02-04 00:44:19 +0500

    push

--- src/Pages/CanadaVisa.js
+++ src/Pages/CanadaVisa.js
@@ -5,7 +5,7 @@ function CanadaVisa() {
     return (
         <>
 
-            <div className='text-center flex items-end pb-5 pl-5 austrialia-banner pt-5'> <span className="text-[56px] pt-11 text-white  font-bold leading-none">
+            <div className='text-center flex items-end pb-5 pl-5 canada-banner pt-5'> <span className="text-[56px] pt-11 text-white  font-bold leading-none">
                 CANADA
             </span>
             </div>
@@ -14,7 +14,7 @@ function CanadaVisa() {
                     <div class="flex justify-center flex-wrap -m-4">
                         <div class="p-4 md:w-full">
                             <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="md:h-[400px] w-full md:w-[50%] object-fill  object-center" src="./491-Regional-Visa-1.png" alt="blog" />
+                                <img class="md:h-[400px] w-full md:w-[50%] object-fill  object-center" src="./canada_PR.jpg" alt="blog" />
                                 <div class="p-6 md:pl-28">
                                     <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">PR VISA</h1>
                                     <p class="leading-relaxed mb-3 text-[20px] text-justify">
@@ -32,7 +32,28 @@ function CanadaVisa() {
                                 </div>
                             </div>
                         </div>
-                        <div class="p-4 md:w-full">
+                        <div class="p-4 md:w-full block md:hidden">
+                            <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <img class="md:h-[400px] w-full md:w-[50%] object-fill  object-center" src="./investor visa canada.png" alt="blog" />
+                            <div class="p-6 md:pr-28">
+                                    <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">INVESTOR VISA</h1>
+                                    <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                        The Investor Visa is designed for individuals who wish to immigrate to Canada by making a significant investment in the country’s economy. This visa is part of Canada’s Start-up Visa Program and other investment pathways, offering entrepreneurs and investors an opportunity to establish businesses, create jobs, or invest in Canadian ventures. By meeting the financial requirements and showing a commitment to supporting Canada’s economic growth, successful applicants can gain permanent residency and enjoy the benefits of living, working, and doing business in Canada.
+
+                                    </p>
+                                    <div class="flex items-center flex-wrap ">
+                                        <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="M5 12h14"></path>
+                                                <path d="M12 5l7 7-7 7"></path>
+                                            </svg>
+                                        </Link>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-full hidden md:block">
                             <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                             <div class="p-6 md:pr-28">
                                     <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">INVESTOR VISA</h1>
@@ -49,13 +70,14 @@ function CanadaVisa() {
                                         </Link>
                                     </div>
                                 </div>
-                                <img class="md:h-[400px] w-full md:w-[50%] object-fill  object-center" src="./Skilled Independent Visa Subclass 189.jpg" alt="blog" />
+                                <img class="md:h-[400px] w-full md:w-[50%] object-fill  object-center" src="./investor visa canada.png" alt="blog" />
                                 
                             </div>
                         </div>
+                       
                         <div class="p-4 md:w-full">
                             <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="h-[400px] w-full md:w-[50%] object-fill  object-center" src="./skilled_nominated_visa_australia.jpg" alt="blog" />
+                                <img class="h-[400px] w-full md:w-[50%] object-fill  object-center" src="./study in canada.jpg" alt="blog" />
                                 <div class="p-6 md:pl-28">
                                     <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">STUDENT VISA </h1>
                                     <p class="leading-relaxed mb-3 text-[20px] text-justify">
@@ -73,10 +95,10 @@ function CanadaVisa() {
                                 </div>
                             </div>
                         </div>
-                        <div class="p-4 md:w-full">
+                        <div class="p-4 md:w-full block md:hidden">
                             <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                                 <img class="h-[400px] w-full md:w-[50%] object-fill  object-center" src="./Canada-visit Visa.webp" alt="blog" />
-                                <div class="p-6 md:pl-28">
+                                <div class="p-6 md:pr-28">
                                     <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">VISIT VISA </h1>
                                     <p class="leading-relaxed mb-3 text-[20px] text-justify">
                                         The Canadian Visit Visa, also known as a Temporary Resident Visa (TRV), allows individuals to travel to Canada for tourism, business, or to visit family or friends. This visa is typically valid for up to six months, depending on the traveler's situation and the discretion of immigration authorities. Applicants must demonstrate that they meet the eligibility requirements, including proof of sufficient funds, ties to their home country, and intent to leave Canada before the visa expires.
@@ -90,6 +112,27 @@ function CanadaVisa() {
                                         </Link>
                                     </div>
                                 </div>
+
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-full hidden md:block">
+                            <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                <div class="p-6 md:pr-28">
+                                    <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">VISIT VISA </h1>
+                                    <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                        The Canadian Visit Visa, also known as a Temporary Resident Visa (TRV), allows individuals to travel to Canada for tourism, business, or to visit family or friends. This visa is typically valid for up to six months, depending on the traveler's situation and the discretion of immigration authorities. Applicants must demonstrate that they meet the eligibility requirements, including proof of sufficient funds, ties to their home country, and intent to leave Canada before the visa expires.
+                                    </p>
+                                    <div class="flex items-center flex-wrap ">
+                                        <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="M5 12h14"></path>
+                                                <path d="M12 5l7 7-7 7"></path>
+                                            </svg>
+                                        </Link>
+                                    </div>
+                                </div>
+                                <img class="h-[400px] w-full md:w-[50%] object-fill  object-center" src="./Canada-visit Visa.webp" alt="blog" />
+
                             </div>
                         </div>
                     </div>
