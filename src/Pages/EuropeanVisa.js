fbfe3ebf9a1f78082ebde07c02823bfa1230c060
Muhammad Haroon <haroonmajid0123@gmail.com>
2025-02-04 19:16:36 +0500

    push

--- src/Pages/EuropeanVisa.js
+++ src/Pages/EuropeanVisa.js
@@ -76,6 +76,162 @@ function EuropeanVisa() {
                     </div>
                 </div>
             </section>
+             <div className='text-center flex items-end pb-5 pl-5 uk-banner pt-5'> <span className="text-[56px] pt-11 text-white  font-bold leading-none">
+                            UNITED KINGDOM
+                        </span>
+                        </div>
+                        <section class="text-gray-600 body-font">
+                            <div class="max-w-screen-xl px-5 py-24 mx-auto">
+                                <div class="flex justify-center flex-wrap -m-4">
+                                    <div class="p-4 md:w-full">
+                                        <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                            <img class="md:h-[400px] w-full md:w-[50%] object-fill  object-center" src="./youth_mobility_visa_united_kingdom.jpeg" alt="blog" />
+                                            <div class="p-6 md:pl-28">
+                                                <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">WORK PERMIT</h1>
+                                                <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                                    This visa allows young people from eligible countries to live and work in the UK for up to two years. Aimed at individuals aged between 18 and 30, this visa provides an opportunity for cultural exchange, travel, and gaining work experience.
+                                                </p>
+                                                <div class="flex items-center flex-wrap ">
+                                                    <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path d="M5 12h14"></path>
+                                                            <path d="M12 5l7 7-7 7"></path>
+                                                        </svg>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="p-4 md:w-full block md:hidden">
+                                        <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                            <img class="md:h-[400px] w-full md:w-[50%] object-fill  object-center" src="./india_young_visa.jpeg" alt="blog" />
+                                            <div class="p-6 md:pr-28">
+                                                <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">INDIA YOUNG PROFESSIONALS VISA</h1>
+                                                <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                                    This visa allows Indian nationals aged between 18 and 30 to live and work in the UK for up to two years. This visa is part of the UK's efforts to strengthen ties with India, offering young professionals the opportunity to gain work experience, explore British culture, and enhance their careers.
+                                                </p>
+                                                <div class="flex items-center flex-wrap ">
+                                                    <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path d="M5 12h14"></path>
+                                                            <path d="M12 5l7 7-7 7"></path>
+                                                        </svg>
+                                                    </Link>
+                                                </div>
+                                            </div>
+            
+                                        </div>
+                                    </div>
+                                    <div class="p-4 md:w-full hidden md:block">
+                                        <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                            <div class="p-6 md:pr-28">
+                                                <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">INDIA YOUNG PROFESSIONALS VISA</h1>
+                                                <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                                    This visa allows Indian nationals aged between 18 and 30 to live and work in the UK for up to two years. This visa is part of the UK's efforts to strengthen ties with India, offering young professionals the opportunity to gain work experience, explore British culture, and enhance their careers.
+            
+                                                </p>
+                                                <div class="flex items-center flex-wrap ">
+                                                    <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path d="M5 12h14"></path>
+                                                            <path d="M12 5l7 7-7 7"></path>
+                                                        </svg>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                            <img class="md:h-[400px] w-full md:w-[50%] object-fill  object-center" src="./india_young_visa.jpeg" alt="blog" />
+            
+                                        </div>
+                                    </div>
+            
+                                    <div class="p-4 md:w-full">
+                                        <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                            <img class="h-[400px] w-full md:w-[50%] object-fill  object-center" src="./global_talent_visa.jpeg" alt="blog" />
+                                            <div class="p-6 md:pl-28">
+                                                <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">GLOBAL TALENT VISA</h1>
+                                                <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                                    This visa is designed for highly skilled individuals in fields such as science, humanities, engineering, medicine, digital technology, and the arts, allowing them to live and work in the UK. This visa offers flexible options, including a pathway to permanent residency, and does not require a job offer.
+            
+                                                </p>
+                                                <div class="flex items-center flex-wrap ">
+                                                    <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path d="M5 12h14"></path>
+                                                            <path d="M12 5l7 7-7 7"></path>
+                                                        </svg>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="p-4 md:w-full block md:hidden">
+                                        <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                            <img class="h-[400px] w-full md:w-[50%] object-fill  object-center" src="./seasonal_worker_visa.jpeg" alt="blog" />
+                                            <div class="p-6 md:pr-28">
+                                                <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">SEASONAL WORKER VISA</h1>
+                                                <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                                    The Seasonal Worker Visa of the UK allows foreign nationals to come to the UK for temporary work in sectors such as agriculture and horticulture. This visa is typically granted for up to six months and is designed to fill labor shortages in specific industries during peak seasons.
+            
+                                                </p>
+                                                <div class="flex items-center flex-wrap ">
+                                                    <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path d="M5 12h14"></path>
+                                                            <path d="M12 5l7 7-7 7"></path>
+                                                        </svg>
+                                                    </Link>
+                                                </div>
+                                            </div>
+            
+                                        </div>
+                                    </div>
+                                    <div class="p-4 md:w-full hidden md:block">
+                                        <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                            <div class="p-6 md:pr-28">
+                                                <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">SEASONAL WORKER VISA</h1>
+                                                <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                                    The Seasonal Worker Visa of the UK allows foreign nationals to come to the UK for temporary work in sectors such as agriculture and horticulture. This visa is typically granted for up to six months and is designed to fill labor shortages in specific industries during peak seasons.
+            
+                                                </p>
+                                                <div class="flex items-center flex-wrap ">
+                                                    <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path d="M5 12h14"></path>
+                                                            <path d="M12 5l7 7-7 7"></path>
+                                                        </svg>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                            <img class="h-[400px] w-full md:w-[50%] object-fill  object-center" src="./seasonal_worker_visa.jpeg" alt="blog" />
+            
+                                        </div>
+                                    </div>
+                                    <div class="p-4 md:w-full">
+                                        <div class="h-full border-2 md:flex border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                            <img class="h-[400px] w-full md:w-[50%] object-fill  object-center" src="./uk_student_visa.webp" alt="blog" />
+                                            <div class="p-6 md:pl-28">
+                                                <h1 class="title-font text-xl font-extrabold text-gray-900 mb-3">STUDENT VISA</h1>
+                                                <p class="leading-relaxed mb-3 text-[20px] text-justify">
+                                                    The Student Visa of the UK allows international students to study at a recognized educational institution in the UK for a specified period. Applicants must have an offer from a UK university or college, demonstrate sufficient funds to cover tuition and living expenses, and meet language proficiency requirements. The visa allows students to work part-time during their studies and full-time during holidays, providing opportunities to gain valuable work experience while pursuing their education.
+            
+            
+                                                </p>
+                                                <div class="flex items-center flex-wrap ">
+                                                    <Link to="/ContactUs" class="text-[#14A660] font-bold hover:text-[#01319F] inline-flex items-center md:mb-2 lg:mb-0">CONTACT NOW
+                                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path d="M5 12h14"></path>
+                                                            <path d="M12 5l7 7-7 7"></path>
+                                                        </svg>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+            
+                                </div>
+                            </div>
+                        </section>
+            
     </>
   )
 }
