28e1e767a5a07c9a6f8797e739061c216d5fc88f
Muhammad Haroon <haroonmajid0123@gmail.com>
2025-02-07 13:31:00 +0500

    up

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -347,7 +347,7 @@ const Contact = () => {
           
         </div>
 
-        <div className="flex justify-between gap-2 mb-6">
+        <div className="md:flex justify-between gap-2 mb-6">
         <ReCAPTCHA
     sitekey="6Le4BMoqAAAAAIBt6QEUKewj_KGnC_XNDMOZl_8H"
     onChange={onChange}
