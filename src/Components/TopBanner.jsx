9b1594f911d29cf906eb340cea9e42270870fae2
Muhammad Haroon <haroonmajid0123@gmail.com>
2025-02-17 00:35:45 +0500

    top banner setting

--- src/Components/TopBanner.jsx
+++ src/Components/TopBanner.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 function TopBanner() {
     return (
         <div className='bg-[#01319F]'>
-            <div className='max-w-screen-2xl text-white flex mx-auto px-4 md:px-0 md:pl-4 justify-between  py-2'>
+            <div className='max-w-screen-2xl text-white flex mx-auto px-4 md:px-0 md:pl-4 justify-between  py-2 md:py-1'>
                 <div className='flex gap-3 md:gap-5 text-sm'>
-                    <div className='flex text-[10px] md:text-l'>
+                    <div className='flex text-[10px] md:text-lg'>
                         <Link to="" className='hover:text-[#14A660]'>
                             <i class="fa-solid fa-phone-flip pr-2"></i>
                             <span>+971 55 905 5498</span>
                         </Link>
                     </div>
-                    <div className='flex text-[10px] md:text-l'>
+                    <div className='flex text-[10px] md:text-lg'>
                         <Link to="" className='hover:text-[#14A660]'>
                             <i class="fa-solid fa-envelope pr-2"></i>
                             <span>info@eiintl.com</span>
