d98f718ccf7b66f8eb1de86232db4707c9edcc95
Muhammad Haroon <haroonmajid0123@gmail.com>
2025-02-17 00:33:35 +0500

    topbanner left side text updated

--- src/Components/TopBanner.jsx
+++ src/Components/TopBanner.jsx
@@ -6,13 +6,13 @@ function TopBanner() {
         <div className='bg-[#01319F]'>
             <div className='max-w-screen-2xl text-white flex mx-auto px-4 md:px-0 md:pl-4 justify-between  py-2'>
                 <div className='flex gap-3 md:gap-5 text-sm'>
-                    <div className='flex text-[10px] md:text-xl'>
+                    <div className='flex text-[10px] md:text-l'>
                         <Link to="" className='hover:text-[#14A660]'>
                             <i class="fa-solid fa-phone-flip pr-2"></i>
                             <span>+971 55 905 5498</span>
                         </Link>
                     </div>
-                    <div className='flex text-[10px] md:text-xl'>
+                    <div className='flex text-[10px] md:text-l'>
                         <Link to="" className='hover:text-[#14A660]'>
                             <i class="fa-solid fa-envelope pr-2"></i>
                             <span>info@eiintl.com</span>
@@ -20,7 +20,7 @@ function TopBanner() {
                     </div>
                 </div>
                 <div className='flex gap-5'>
-                    <a href='https://www.facebook.com/profile.php?id=61572185520178' target='_blank'><i class="fa-brands fa-facebook-f hover:text-[#14A660]"></i></a>
+                    <a href='https://www.facebook.com/profile.php?id=61572185520178' target='_blank'><i class="fa-brands fa-facebook-f hover:text-[#060b09]"></i></a>
                     <a  href='https://www.instagram.com/entrustimmigration/' target="_blank"><i class="fa-brands fa-instagram hover:text-[#14A660]"></i></a>
                     <a href=''><i class="fa-brands fa-linkedin-in hover:text-[#14A660]"></i></a>
                 </div>
