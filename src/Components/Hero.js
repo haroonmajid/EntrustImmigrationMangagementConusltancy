3e6244668ba42fac4142db4fc342450a88bdec6d
Muhammad Haroon <haroonmajid0123@gmail.com>
2025-02-17 15:30:06 +0500

    I updated some thing

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
    const [isExpanded, setIsExpanded] = useState(false);
@@ -25,7 +26,7 @@ export default function Hero() {
       {/* SKILLED IMMIGRATION */}
       <div className="text-center md:hidden max-w-screen-2xl mx-auto px-6">
         <h1 className="text-[#01319f] font-bold leading-none text-[36px] md:text-[64px]  mb-6">
-          <span className="text-[#14A660]">SKILLED</span> <br/> IMMIGRATION
+          SKILLED <br/><span className="text-[#14A660]"> IMMIGRATION</span>
         </h1>
         <img src="/pic-2.PNG" alt="Skilled Immigration" className="w-full max-w-screen-lg mx-auto" />
         <div className="w-[90%] md:w-[85%] text-justify text-[16px] md:text-[20px] leading-relaxed max-w-5xl mx-auto mt-10">
@@ -40,16 +41,17 @@ export default function Hero() {
           </button>
         </div>
         
+        <Link to="/ContactUs">
         <button
             type="submit"
             className="w-full md:w-1/3 py-3 px-6 mb-5 font-semibold text-[#01319f] rounded-md hover:bg-[#01319f] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#01319f] text-[18px] border-[1px] border-solid border-[#14A660]"
           >
             Learn More
-          </button>
+          </button></Link>
       </div>
       <div className="text-center md:hidden max-w-screen-2xl mx-auto py-12 px-6">
         <h1 className="text-[#01319f] font-bold leading-none text-[36px] md:text-[64px]  mb-6">
-          <span className="text-[#14A660]">WORK PERMIT</span> <br/> IMMIGRATION
+         WORK PERMIT <br/> <span className="text-[#14A660]"> IMMIGRATION</span>
         </h1>
         <img src="/work_permit_banner.jpg" alt="Skilled Immigration" className="w-full max-w-screen-lg mx-auto" />
         <div className="w-[90%] md:w-[85%] text-justify text-[16px] md:text-[20px] leading-relaxed max-w-5xl mx-auto mt-10">
@@ -64,16 +66,17 @@ export default function Hero() {
           </button>
         </div>
         
+        <Link to="/ContactUs">
         <button
             type="submit"
             className="w-full md:w-1/3 py-3 px-6 mb-5 font-semibold text-[#01319f] rounded-md hover:bg-[#01319f] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#01319f] text-[18px] border-[1px] border-solid border-[#14A660]"
           >
             Learn More
-          </button>
+          </button></Link>
       </div>
       <div className="text-center md:hidden max-w-screen-2xl mx-auto px-6">
         <h1 className="text-[#01319f] font-bold leading-none text-[36px] md:text-[64px]  mb-6">
-          <span className="text-[#14A660]">TOURIST</span> <br/> VISA
+          TOURIST <br/><span className="text-[#14A660]"> VISA</span>
         </h1>
         <img   src="./tourist_visa_banner.jpg" alt="Skilled Immigration" className="w-full max-w-screen-lg mx-auto" />
         <div className="w-[90%] md:w-[85%] text-justify text-[16px] md:text-[18px] leading-relaxed max-w-5xl mx-auto mt-10">
@@ -88,12 +91,13 @@ export default function Hero() {
           </button>
         </div>
         
+        <Link to="/ContactUs">
         <button
             type="submit"
             className="w-full md:w-1/3 py-3 px-6 mb-5 font-semibold text-[#01319f] rounded-md hover:bg-[#01319f] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#01319f] text-[18px] border-[1px] border-solid border-[#14A660]"
           >
             Learn More
-          </button>
+          </button></Link>
       </div>
 
       {/* Section 1 */}
@@ -105,17 +109,18 @@ export default function Hero() {
         />
         <div className="text-center md:text-left py-6">
           <span className="text-[42px] text-[#01319f] font-bold leading-tight">
-            <span className="text-[#14A660]">SKILLED</span> <br /> IMMIGRATION
+            SKILLED <br /><span className="text-[#14A660]"> IMMIGRATION</span>
           </span>
           <p className="text-[16px] py-6 sm:py-4 text-justify">
           At Entrust Immigration Management Consultancy, we specialize in guiding skilled professionals toward securing opportunities in some of the world's most sought-after destinations. Skilled immigration programs are designed to attract individuals with the expertise, qualifications, and work experience that contribute to economic growth and innovation.
           </p>
-          <button
+          <Link to="/ContactUs">
+        <button
             type="submit"
             className="w-full md:w-1/3 py-3 px-6 mb-5 font-semibold text-[#01319f] rounded-md hover:bg-[#01319f] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#01319f] text-[18px] border-[1px] border-solid border-[#14A660]"
           >
             Learn More
-          </button>
+          </button></Link>
           
         </div>
       </div>
@@ -125,17 +130,18 @@ export default function Hero() {
         <div className="text-center md:text-left py-6">
           <sp363
           an className="text-[42px] text-[#01319f] font-bold leading-tight">
-            <span className="text-[#14A660]">WORK PERMIT</span> <br /> IMMIGRATION
+            WORK PERMIT <br /><span className="text-[#14A660]"> IMMIGRATION</span>
           </sp363>
           <p className="text-[16px] py-11 sm:py-4 text-justify">
           Unlock global career opportunities with Entrust Immigration Management Consultancy. Work permit immigration allows individuals to live and work abroad while gaining international experience and advancing their professional growth. Whether you’re a skilled worker, a professional, or someone seeking short-term work opportunities, we’re here to guide you every step of the way.
           </p>
-          <button
+          <Link to="/ContactUs">
+        <button
             type="submit"
             className="w-full md:w-1/3 py-3 px-6 mb-5 font-semibold text-[#01319f] rounded-md hover:bg-[#01319f] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#01319f] text-[18px] border-[1px] border-solid border-[#14A660]"
           >
             Learn More
-          </button>
+          </button></Link>
         </div>
         <img
           src="./work_permit_banner.jpg"
@@ -153,17 +159,18 @@ export default function Hero() {
         />
         <div className="text-center md:text-left py-6">
           <span className="text-[42px] text-[#01319f] font-bold leading-tight">
-            <span className="text-[#14A660]">TOURIST</span> <br /> VISA
+            TOURIST <br /><span className="text-[#14A660]"> VISA</span>
           </span>
           <p className="text-[16px] py-6 sm:py-4 text-justify">
           Explore the world with ease through Entrust Immigration Management Consultancy. A tourist visa is your gateway to experiencing breathtaking destinations, diverse cultures, and unforgettable adventures. Whether you're planning a vacation, visiting loved ones, or exploring new horizons, we’re here to make the process seamless and stress-free.
           </p>
-          <button
+          <Link to="/ContactUs">
+        <button
             type="submit"
             className="w-full md:w-1/3 py-3 px-6 mb-5 font-semibold text-[#01319f] rounded-md hover:bg-[#01319f] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#01319f] text-[18px] border-[1px] border-solid border-[#14A660]"
           >
             Learn More
-          </button>
+          </button></Link>
         </div>
       </div>
     </div>
